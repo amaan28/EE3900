7c515bff213b1976ae6f58770d8b0b5cf8834304
amaan28 <74655510+amaan28@users.noreply.github.com>
2022-08-12 11:52:06 +0530

    Update Ex_3.1.py

--- codes/Ex_3.1.py
+++ codes/Ex_3.1.py
@@ -1,10 +1,22 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-n=np.array([0,1,2,3,4,5])
-x=np.array([1,2,3,4,2,1])
-plt.xlabel('$n$')
+x=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
+k = 20
+y = np.loadtxt("y.txt",dtype=np.double)
+print(y)
+
+#Subplots
+plt.subplot(2, 1, 1)
+plt.stem(range(0,6),x)
+plt.title('Digital Filter Input-Output')
 plt.ylabel('$x(n)$')
-plt.stem(n,x)
+plt.grid()
+
+
+plt.subplot(2, 1, 2)
+plt.stem(range(0,k),y)
+plt.xlabel('$n$')
+plt.ylabel('$y(n)$')
 plt.grid()
 plt.show()
